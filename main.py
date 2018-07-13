035ea5020191b24b91a3eb2a6d3a0d443cf094cf
Artem Biluta <Artem.Biluta@south.rt.ru>
2018-07-13 11:06:06 +0300

    правка замечаний

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import requests
+from config import token
 
 url = 'https://api.telegram.org/bot'
-token = '609015270:AAEFnNJwGdYpi49_Vw_SQZRxIwCV5aREl3w'
+bot_msg = 'Привет! Я суперпупер бот, который покажет тебе стоимоть бетховенов и эфира в деревянных =)\nСписок команд:\n - /btc - цена за бетховен\n - /eth - цена за эфир'
 
 
 def get_bot_updates(limit, offset):
-    req = url + token + '/getUpdates'
+    method_url = url + token + '/getUpdates'
     par = {'limit': limit, 'offset': offset}
-    result = requests.get(req, params=par)
+    result = requests.get(method_url, params=par)
     # форматируем json в словарь
     decoded = result.json()
     return decoded['result']
 
 
 def sendMessageBot(text, chat_id):
-    req = url + token + '/sendMessage'
+    method_url = url + token + '/sendMessage'
     par = {'chat_id': chat_id, 'text': text}
-    requests.post(req, params=par)
+    requests.post(method_url, params=par)
 
 
-def get_coin_rub(coin):
-    param = coin + '-rub'
-    req = 'https://api.cryptonator.com/api/ticker/'+param
-    result = requests.get(req)
+def get_coin_rub(coin_rub):
+    param = coin_rub + '-rub'
+    method_url = 'https://api.cryptonator.com/api/ticker/'+param
+    result = requests.get(method_url)
     decoded = result.json()
     result = decoded['ticker']
     return result['price']
 
+def bot_return(offset):
+    result = get_bot_updates(5, offset)
+    chat_id = item['message']['chat']['id']
+    text = item['message']['text']
+    if text == '/start':
+        text = bot_msg
+        sendMessageBot(text, chat_id)
+    elif text == '/btc':
+        coin = get_coin_rub('btc')
+        text = 'бетховены опять упали, стоят ' + coin + ' руб.'
+        sendMessageBot(text, chat_id)
+    elif text == '/eth':
+        coin = get_coin_rub('eth')
+        text = 'эфир сегодня ' + coin + ' руб.'
+        sendMessageBot(text, chat_id)
 
 while True:
     try:
@@ -34,22 +50,7 @@ while True:
         for item in result:
             update_id = item['update_id']
             new_offset = update_id + 1
-            result = get_bot_updates(5, new_offset)
-            chat_id = item['message']['chat']['id']
-            text = item['message']['text']
-            if text == '/start':
-                text = 'Привет! Я суперпупер бот, который покажет тебе стоимоть бетховенов и эфира в деревянных =)\nСписок команд:\n - /btc - цена за бетховен\n - /eth - цена за эфир'
-                sendMessageBot(text, chat_id)
-            elif text == '/btc':
-                coin = get_coin_rub('btc')
-                text = 'бетховены опять упали, стоят ' + coin + ' руб.'
-                sendMessageBot(text, chat_id)
-            elif text == '/eth':
-                coin = get_coin_rub('eth')
-                text = 'эфир сегодня ' + coin + ' руб.'
-                sendMessageBot(text, chat_id)
-            else:
-                continue
+            bot_return(new_offset)
     except KeyboardInterrupt:  # порождается, если бота остановил пользователь
         print('Interrupted by the user')
         break
